16355c39edaa41c9a4c94e8eda3f2221256fcf0a
Antoniue <Verstiplayingtm@gmail.com>
2022-09-10 13:57:26 +0300

    added a drawBooks function to add the books to the HTML doc

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ function addBookToLibrary(myLibrary,book)
 {
     myLibrary.push(book);
     bookCounter++;
+    drawBooks(myLibrary[bookCounter-1]);
 }
+
+function drawBooks(book)
+{
+    let htmlTemplate = '<div class="singleBook">'
+    + '<div class="bookNumber">Book #'+bookCounter+'</div>'
+    + '<div class="bookContents">'
+    + '<div class="bookTitle">Title: '+book.title+'</div>'
+    + '<div class="bookAuthor">Author: '+book.author+'</div>'
+    + '<div class="bookPages">Pages: '+book.pages+'</div>'
+    + '<div class="bookRead">Read: '+book.isRead+'</div>'
+    + '</div>'
+    +'</div>'
+    let booksContainer = document.querySelector('.booksContainer');
+    booksContainer.innerHTML = booksContainer.innerHTML + htmlTemplate;
+}
+
 
 
 
