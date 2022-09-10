7aff842942e17b1e365aabf4e1dc31bd72df3c13
Antoniue <Verstiplayingtm@gmail.com>
2022-09-10 15:15:24 +0300

    added remove book functionality

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ function Book(title, author, pages, isRead)
 function addBookToLibrary(myLibrary,book)
 {
     myLibrary.push(book);
-    bookCounter++;
+}
+
+function removeBookFromLibrary(bookIndex)
+{
+    myLibrary.splice(bookIndex,1);
+    booksContainer.innerHTML ='';
+    drawBooks(myLibrary);
 }
 
 function drawRead(book)
@@ -25,27 +31,39 @@ function drawBooks(library)
     for(let index = 0; index < library.length; index++)
     {
     let htmlTemplate = '<div class="singleBook">'
-    + '<div class="bookNumber">Book #'+bookCounter+'</div>'
+    + '<div class="bookNumber">Book #'+(index+1)+'</div>'
     + '<div class="bookContents">'
     + '<div class="bookTitle">Title: '+library[index].title+'</div>'
     + '<div class="bookAuthor">Author: '+library[index].author+'</div>'
     + '<div class="bookPages">Pages: '+library[index].pages+'</div>'
     + '<div class="bookRead" '+drawRead(library[index])+'>Read</div>'
     + '<div class="bookButtons">'
-    + '<div class="removeBook">Remove Book</div>'
+    + '<div class="removeBook" '+'id="1"'+'>Remove Book</div>'
     + '<div class="changeRead">Change Read</div>'
     + '</div>'
     + '</div>'
     +'</div>'
-    let booksContainer = document.querySelector('.booksContainer');
     booksContainer.innerHTML = booksContainer.innerHTML + htmlTemplate;
     }
 }
 
-let bookCounter = 0;
+let booksContainer = document.querySelector('.booksContainer');
 let myLibrary = [];
-// addBookToLibrary(myLibrary, new Book('test','test',10,true));
-// addBookToLibrary(myLibrary, new Book('test','test',10,false));
-// addBookToLibrary(myLibrary, new Book('test','test',10,true));
-// addBookToLibrary(myLibrary, new Book('test','test',10,false));
-// drawBooks(myLibrary);
+addBookToLibrary(myLibrary, new Book('test','test',10,true));
+addBookToLibrary(myLibrary, new Book('test','test',10,false));
+addBookToLibrary(myLibrary, new Book('test','test',10,true));
+addBookToLibrary(myLibrary, new Book('test','test',10,false));
+drawBooks(myLibrary);
+
+const removeBook = document.querySelectorAll('.removeBook');
+const changeRead = document.querySelectorAll('.changeRead');
+for(let index = 0; index < removeBook.length; index++)
+{
+    removeBook[index].addEventListener(
+        'click',
+        function()
+        {
+            removeBookFromLibrary(index);
+        }
+    );
+}
